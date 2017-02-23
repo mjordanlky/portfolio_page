9273b65393924a85500e9b47f260b9262e57fada
Jordan Larimore <jordanarimore@Matthews-MacBook-Air.local>
2017-02-23 18:13:44 -0500

    Initial Commit

---
+++ jS/script.js
@@ -0,0 +1,29 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+
+/*
+jQuery(document).ready(function () {
+    "use strict";
+    jQuery(".menuTrigger").click(function () {
+        
+        jQuery(".mainMenu").slideToggle();
+    });
+    
+});
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
